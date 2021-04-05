9b0412de324fca93c66a1e73004cb55053db9e38
NaoshiAC <naoshiarimori@gmail.com>
2021-04-05 16:18:22 -0300

    Fisinished home page stylization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +28,56 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | SpaceTraveling</title>
+      </Head>
+
+      <main className={commonStyles.container}>
+        <div className={styles.posts}>
+
+          {/* <Link href={`/post/${post.uid}`} key={post.uid}> */}
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida. Pensando em sincronização em vez de ciclos de vida.</p>
+
+            <section>
+              <div>
+                <FiCalendar size={'1.25rem'} className={styles.icon} />
+                <time>06 de março de 2021</time>
+              </div>
+
+              <div>
+                <FiUser size={'1.25rem'} className={styles.icon} />
+                <span>Naoshi Arimori Cyrilli</span>
+              </div>
+            </section>
+          </a>
+
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+
+            <section>
+              <div>
+                <FiCalendar size={'1.25rem'} className={styles.icon} />
+                <time>06 de março de 2021</time>
+              </div>
+
+              <div>
+                <FiUser size={'1.25rem'} className={styles.icon} />
+                <span>Naoshi Arimori Cyrilli</span>
+              </div>
+            </section>
+          </a>
+          {/* </Link> */}
+        </div>
+      </main>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
