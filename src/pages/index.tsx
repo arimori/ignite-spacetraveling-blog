b09d89f703f99fde5bbe367b9a014dbeeea37d36
NaoshiAC <naoshiarimori@gmail.com>
2021-04-05 21:22:15 -0300

    Integrated with Prismic CMS
    - Created integration with Prismic CMS on home page;

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
-import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useState } from 'react';
+import Prismic from '@prismicio/client';
+import { GetStaticProps } from 'next';
+import { getPrismicClient } from '../services/prismic';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { FiCalendar, FiUser } from 'react-icons/fi';
-
-import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
 interface Post {
   uid?: string;
+  first_publication_date_formatted: string | null;
   first_publication_date: string | null;
   data: {
     title: string;
@@ -26,9 +30,38 @@ interface PostPagination {
 
 interface HomeProps {
   postsPagination: PostPagination;
+  next_page: string;
 }
 
-export default function Home() {
+export default function Home({ postsPagination }: HomeProps) {
+  const [posts, setPosts] = useState(postsPagination.results);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+
+  async function handleLoadMorePosts() {
+    const { results } = await fetch(nextPage)
+      .then(response => response.json());
+
+    const newPosts = results.map(post => {
+      return {
+        uid: post.uid,
+        first_publication_date: post.first_publication_date,
+        first_publication_date_formatted: format(
+          new Date(post.first_publication_date),
+          "dd MMM yyyy",
+          { locale: ptBR }
+        ),
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author,
+        }
+      }
+    });
+
+    setPosts([...posts, ...newPosts]);
+    setNextPage(results.data?.next_page);
+  }
+
   return (
     <>
       <Head>
@@ -37,51 +70,75 @@ export default function Home() {
 
       <main className={commonStyles.container}>
         <div className={styles.posts}>
+          {posts.map(post => (
+            <Link href={`/post/${post.uid}`} key={post.uid}>
+              <a>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
 
-          {/* <Link href={`/post/${post.uid}`} key={post.uid}> */}
-          <a>
-            <strong>Como utilizar Hooks</strong>
-            <p>Pensando em sincronização em vez de ciclos de vida. Pensando em sincronização em vez de ciclos de vida.</p>
+                <section>
+                  <div>
+                    <FiCalendar size={'1.25rem'}/>
+                    <time>{post.first_publication_date_formatted}</time>
+                  </div>
 
-            <section>
-              <div>
-                <FiCalendar size={'1.25rem'} className={styles.icon} />
-                <time>06 de março de 2021</time>
-              </div>
+                  <div>
+                    <FiUser size={'1.25rem'} />
+                    <span>{post.data.author}</span>
+                  </div>
+                </section>
+              </a>
+            </Link>
+          ))}
+          {nextPage && (
+            <button
+              type="button"
+              className={styles.loadPosts}
+              onClick={() => handleLoadMorePosts()}
+            >
+              Carregar mais posts
+            </button>
+          )}
 
-              <div>
-                <FiUser size={'1.25rem'} className={styles.icon} />
-                <span>Naoshi Arimori Cyrilli</span>
-              </div>
-            </section>
-          </a>
-
-          <a>
-            <strong>Como utilizar Hooks</strong>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
-
-            <section>
-              <div>
-                <FiCalendar size={'1.25rem'} className={styles.icon} />
-                <time>06 de março de 2021</time>
-              </div>
-
-              <div>
-                <FiUser size={'1.25rem'} className={styles.icon} />
-                <span>Naoshi Arimori Cyrilli</span>
-              </div>
-            </section>
-          </a>
-          {/* </Link> */}
         </div>
       </main>
     </>
   )
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'publication')
+  ], {
+    fetch: ['publication.title', 'publication.subtitle', 'publication.author'],
+    pageSize: 2,
+  });
+
+  const results = response.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      first_publication_date_formatted: format(
+        new Date(post.first_publication_date),
+        "dd MMM yyyy",
+        { locale: ptBR }
+      ),
+      data: {
+        title: post.data.title,
+        subtitle: post?.data.subtitle,
+        author: post.data.author,
+      }
+    }
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        results,
+        next_page: response.next_page
+      }
+    }
+  }
+};
--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -2,9 +2,11 @@ import Prismic from '@prismicio/client';
 import { DefaultClient } from '@prismicio/client/types/client';
 
 export function getPrismicClient(req?: unknown): DefaultClient {
-  const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT, {
-    req,
-  });
+  const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT,
+    {
+      req
+    }
+  );
 
   return prismic;
 }
